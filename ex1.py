5512c73d62ccdac26bd8df4fafbcab9dad6d8ff2
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-03-07 15:14:05 +0300

    Delete ex1.py

--- ex1.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Kaç sayı girmek istiyorsunuz ? "))
-total_sum = 0
-
-for n in range(num):
-    numbers = float(input("Sayı giriniz : "))
-    total_sum += numbers
-
-avg = total_sum / num
-print("Average is : ",avg)
